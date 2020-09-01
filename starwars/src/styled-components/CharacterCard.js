d9ac723307eaabc455f5a27da6521edcabe0beab
Mychael M <mychaelbmenges@gmail.com>
2020-08-31 18:26:57 -0600

    added animation to cards when loading page

--- starwars/src/styled-components/CharacterCard.js
+++ starwars/src/styled-components/CharacterCard.js
@@ -1,4 +1,15 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+export const cardOpacity = keyframes`
+
+  0% {
+    opacity: 0
+  }
+  100% {
+    opacity: 1;
+  }
+
+`
 
 export const CharacterCard = styled.div`
   border: 1px solid black;
@@ -8,4 +19,5 @@ export const CharacterCard = styled.div`
   width: 25%;
   // padding: 3%;
   margin: 3%;
+  animation: ${cardOpacity} 1.5s ease-in 0s 1;
 `;
